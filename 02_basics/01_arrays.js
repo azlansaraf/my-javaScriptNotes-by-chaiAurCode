d8d306d9cb1d3b043dd681423fb14dae68017087
azlansaraf <sarafazlan@gmail.com>
2024-02-28 09:46:00 -0800

    initial files

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,25 +4,25 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeros = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[0]);
+console.log(myArr[0]);
 
 // Array methods
 
-// myArr.push(6) // to add new arrays in array
-// myArr.push(7)
-// myArr.pop() // to remove last value in array
-// myArr.unshift(9) //this will add value at start of the array
-// myArr.shift() //this will remove value at start of the array
+myArr.push(6) // to add new arrays in array
+myArr.push(7)
+myArr.pop() // to remove last value in array
+myArr.unshift(9) //this will add value at start of the array
+myArr.shift() //this will remove value at start of the array
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(9));
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
 
 
-// const newArr = myArr.join() //adds all the elements of an array into a string, separated by the specified separator string.
-// console.log(myArr);
-// console.log(newArr);
+const newArr = myArr.join() //adds all the elements of an array into a string, separated by the specified separator string.
+console.log(myArr);
+console.log(newArr);
 
-// slice, splice
+slice, splice
 
 console.log("A ", myArr);
 
